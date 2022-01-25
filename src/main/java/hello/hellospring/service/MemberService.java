d810da3ce06972d598f9919a0640760c3d6a7d04
LeeJeongGi <apple4rhk@naver.com>
2022-01-25 20:19:36 +0900

    refactor - 사용안하는 import 제거

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,7 +2,6 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
