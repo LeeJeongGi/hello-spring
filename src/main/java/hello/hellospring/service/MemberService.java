f387402668d1f22b6c85c7da47c3a56519c35bd1
LeeJeongGi <apple4rhk@naver.com>
2022-01-25 20:30:40 +0900

    refactor - 컴포넌트 스캔과 자동 의존관계 설정

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,14 +2,18 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     //test 클래스 자동으로 만드는 단축키 : 커맨드 + 쉬프트 + t
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
