05a0d7d708266c175ae78080bebda73c3033733d
LeeJeongGi <apple4rhk@naver.com>
2022-01-30 21:55:03 +0900

    refactor : 소스 정리

--- src/main/java/hello/hellospring/aop/TimeTraceAop.java
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -11,13 +11,17 @@ public class TimeTraceAop {
 
     @Around("execution(* hello.hellospring..*(..))")
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+
         long start = System.currentTimeMillis();
+
         System.out.println("START: " + joinPoint.toString());
+
         try {
             return joinPoint.proceed();
         } finally {
             long finish = System.currentTimeMillis();
             long timeMs = finish - start;
+
             System.out.println("END: " + joinPoint.toString()+ " " + timeMs + "ms");
         }
     }
